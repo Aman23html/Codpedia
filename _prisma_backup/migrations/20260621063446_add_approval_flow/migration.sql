3e5ac4338b1daa836dc216d9adc4f75463c701ce
Aman23html <aman.atka2345@gmail.com>
2026-07-12 17:03:39 +0530

    prisma remove

--- _prisma_backup/migrations/20260621063446_add_approval_flow/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "approvedById" TEXT;
-
--- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_approvedById_fkey" FOREIGN KEY ("approvedById") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
